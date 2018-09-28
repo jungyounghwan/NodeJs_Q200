a49bee497dd2055f500980c1115dab303db6daff
jung <jnoah80@gmail.com>
2018-09-28 17:57:59 +0900

    ~q010

---
+++ part01/q005.js
@@ -0,0 +1,7 @@
+console.log('name:%s', 'jung'); // 템플릿 형식으로 출력하기
+console.log('age:%d', 31); // 숫자는 %d
+console.log('pi:%d', 3.14);
+console.log('math:%d science:%d', 92, 84); // 2가지 같이 쓰기
+console.log('name:%s age:%d', 'jung', 31); // 여러가지 같이쓰기
+console.log('name:%s math:%d science:%d', 'jung', 92, 84); // 3가지 같이 쓰기
+console.log('name:%s math:%s science:%s', 'jung', 92, 84); // %s로 다 쓰기
